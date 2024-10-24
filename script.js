e23b2a5c805ca7d3e6b2ac7bff18479d5964ddb2
Alfredo Limin <jjlimin@yahoo.com>
2024-10-24 21:27:37 +0300

    minor changes

--- script.js
+++ script.js
@@ -5,18 +5,3 @@ navigator.mediaDevices.getUserMedia( { video: true } ).then((stream) => {
 }).catch((error) => {
     console.error(error);
 })
-
-// snapButton.addEventListener('click', () => {
-//     canvas.width = video.videoWidth;
-//     canvas.height = video.videoHeight;
-//     const ctx = canvas.getContext('2d');
-    
-//     // Draw the video frame on the canvas
-//     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    
-//     // Draw the overlay frame
-//     ctx.drawImage(overlay, 0, 0, canvas.width, canvas.height);
-    
-//     // Show the canvas (snapshot)
-//     canvas.style.display = 'block';
-// });
